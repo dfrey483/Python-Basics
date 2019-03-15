b769b2ef3acab18a7ee95f73efa0a12d1a3d726b
Vikranth Inti <vikranth.info@gmail.com>
2019-03-15 13:03:29 +0530

    Recent Commit

---
+++ Basics/DateTime.py
@@ -0,0 +1,9 @@
+'''Write a Python program to display the current date and time.'''
+
+from datetime import date
+from datetime import time
+from datetime import datetime
+Tdate=date.today()
+print(Tdate)
+Tnow=datetime.now()
+print(Tnow)
