d4135a3967787fdb49bf784005dfefadb2a45147
Vikranth Inti <vikranth.info@gmail.com>
2019-03-15 18:25:37 +0530

    Recent Commit

---
+++ Basics/ListTuple.py
@@ -0,0 +1,6 @@
+'''Write a Python program which accepts a sequence of comma-separated numbers
+from user and generate a list and a tuple with those numbers.'''
+
+NumList=input("Enter numbers separated by space : ")
+print("List of given numbers is : "+NumList)
+print("Tuple of given numbers is : "+str(tuple(NumList)))
