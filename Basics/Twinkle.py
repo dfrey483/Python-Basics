536f0595a5e61462975436bbc8236546d293e88d
Vikranth Inti <vikranth.info@gmail.com>
2019-03-15 12:30:13 +0530

    Recent Commit

---
+++ Basics/Twinkle.py
@@ -0,0 +1,16 @@
+'''Write a Python program to print the following string in a specific format (see the output). Go to the editor
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are'''
+
+
+
+print "Twinkle , twinkle, little star,"
+print " \t\t How I wonder what you are!"
+print "\t\t\t Up above the world so high,"
+print "\t\t\t Like a diamond in the sky."
