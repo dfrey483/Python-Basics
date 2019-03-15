891b5bce62361ca21ed4fc0aa8813c7456ad7f8e
Vikranth Inti <vikranth.info@gmail.com>
2019-03-15 13:35:15 +0530

    Recent Commit

---
+++ Basics/ReverseName.py
@@ -0,0 +1,6 @@
+'''Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.'''
+
+name=input("Please give your full name : ")
+ListName=name.split(' ')
+outputString=ListName[1]+' '+ListName[0]
+print(outputString)
