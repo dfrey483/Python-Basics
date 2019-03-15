c77c4c059cdec89c953add1af060af867f6f883b
Vikranth Inti <vikranth.info@gmail.com>
2019-03-15 13:13:23 +0530

    Recent Commit

---
+++ Basics/Circle.py
@@ -0,0 +1,6 @@
+'''Write a Python program which accepts the radius of a circle from the user and compute the area.'''
+
+radius=float(input("Please enter the radius of  circle : "))
+area=float(22/7)*radius**2
+area=str(area)
+print("Area of the given circle is : "+area)
