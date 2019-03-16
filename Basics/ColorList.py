b850aaf357bf108866f196e0f5559df259b7c724
Vikranth Inti <vikranth.info@gmail.com>
2019-03-16 12:18:39 +0530

    Recent Commit

---
+++ Basics/ColorList.py
@@ -0,0 +1,8 @@
+'''Write a Python program to display the first and last colors from the following list. 
+color_list = ["Red","Green","White" ,"Black"]'''
+
+color_list=["red","green","white","black"]
+print("Given color list is : "+str(color_list))
+length=len(color_list)
+print("First color of the list is : "+str(color_list[0]))
+print("Last color of the list is : "+str(color_list[length-1]))
