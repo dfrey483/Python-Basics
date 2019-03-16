3a82ceee34bb0ef682b03b8ce8170f701e9d4620
Vikranth Inti <vikranth.info@gmail.com>
2019-03-16 12:52:30 +0530

    Recent Commit

---
+++ Basics/N.py
@@ -0,0 +1,4 @@
+'''Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn'''
+
+n=float(input("Please enter a number : "))
+print("The required value is : "+str(n+n*n+n**3))
