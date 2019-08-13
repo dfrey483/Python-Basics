446bd791b281a98607bfc56cc3721596afba039c
Vikranth Inti <vikranth.info@gmail.com>
2019-08-13 12:22:34 +0530

    Python code for exercise 1 in Arrays
    1.py

---
+++ Arrays/1.py
@@ -0,0 +1,10 @@
+'''Write a Python program to create an array of 5 integers and display the array items.
+Access individual element through indexes.
+Go to the editor Click me to see the sample solution'''
+
+from array import array
+
+A = [1,'d',54,'g','z']
+
+for i in A:
+    print (i)
